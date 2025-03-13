da5d1d760ef1cfe4fb987b4f50d6d50a59ebb0df
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-03-13 17:50:32 -0300

    produtos adicionados
    novos produtos adicionados

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -9,28 +9,28 @@ const Gallery = ({images}) => {
         setSelectImg(index);
     }
     
-    
     return (
     <section className={styles.wrapper}>
         <div className={styles.containerMain}>
-            <img src={images[selectImg].img} alt={images.AltImg} />
+            <img src={images[selectImg].img} alt={images[selectImg].AltImg} />
         </div>
 
         <div className={styles.ContainerSecondary}>
             {
                 images.map ((item, index) => {
                     return(
-                    <figure
+                    <div
+                        key={index}
                         onClick={() => changeImg(index)}
-                        className={styles.containerImg}>
+                        className={`${styles.containerImg} ${index === selectImg ? styles.active : ''}`}>
                         <img src={item.img} alt={item.AltImg} />
-                    </figure>
+                    </div>
                     )
                 })
             }
         </div>
     </section>
-    )
+    );
 }
 
 export default Gallery
