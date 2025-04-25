ab18167d76aed6f9c12710349697358e510f12f2
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-04-24 23:37:25 -0300

    style: responsividade do CategorySection

--- src/components/CategorySection/index.jsx
+++ src/components/CategorySection/index.jsx
@@ -1,3 +1,10 @@
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css/bundle';
+
 import styles from './category-section.module.scss';
 import dataProduct from '../../data/DataProduct.json';
 import StoriesInsta from '../Stories';
@@ -7,11 +14,6 @@ import { data, Link, useNavigate } from "react-router-dom";
 import CardSearch from '../CardSearch';
 import slugify from 'slugify';
 import MediaQuery from '../../utils/MediaQuery/MediaQuery';
-
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
-import 'swiper/css';
 
 const CategorySection =  React.forwardRef(({ category, text }, ref) => {
 
@@ -26,22 +28,38 @@ const CategorySection =  React.forwardRef(({ category, text }, ref) => {
     className={styles.containerCategory}
     ref={ref}
     >
-        <h2>{category}</h2>
-        <p>{text}</p>
+        <div className={styles.containerInfo}>
+          <h2>{category}</h2>
+          <p>{text}</p>
+        </div>
 
         
-        {isMobile && <StoriesInsta filter={category} />}
-        {!isMobile && <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-
+        {isMobile &&
+        <section className={styles.containerStories}>
+          <StoriesInsta filter={category} />
+        </section>
+        }
+        {!isMobile &&
+        <section className={styles.containerSwiper}>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            navigation={true}
+            pagination={true}
+            loop={true}
+            modules={[Autoplay]}
+            autoplay={{
+              delay: 3000, // 3 segundos
+              disableOnInteraction: false, // continua mesmo com interações
+            }}
+          >
             <SwiperSlide><img src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745524625/Nec_Ev_-_juta_-_banner_-_site_exyizg.jpg" alt="" /></SwiperSlide>
             <SwiperSlide><img src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745525938/Nec_Ev_-_juta_-_banner_-_site_mhfegb.jpg" alt="" /></SwiperSlide>
             <SwiperSlide><img src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745526109/Nec_Ev_-_juta_-_banner_-_site_qtgca6.jpg" alt="" /></SwiperSlide>
-          </Swiper>}
+          </Swiper>
+        </section>}
 
         <div className={styles.containerCard}>
           {dataProduct
