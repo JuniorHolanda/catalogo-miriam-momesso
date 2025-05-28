83e2916515499a8e2642621475bd8b2148ff429e
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-05-28 17:49:35 -0300

    style: melhora o estilo do navBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,22 +5,24 @@ const NavBar = () => {
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.containerLogo}>
-				<img
-					className={styles.logo}
-					src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745342991/logo-novo-para-fundo-black_gbm10y.svg"
-					alt="logo da empresa Momesso Brindes"
-				/>
+				<Link to={'/'} >
+					<img 
+						className={styles.logo}
+						src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745342991/logo-novo-para-fundo-black_gbm10y.svg"
+						alt="logo da empresa Momesso Brindes"
+					/>
+				</Link>
 			</div>
 			<nav className={styles.navMenu}>
 				<ul className={styles.listMenu}>
-					<li className={styles.menuItem}>
-						<button>Home</button>
+					<li className={styles.containerMenuItem}>
+						<Link className={styles.menuItem} to={'/'} >Home</Link>
 					</li>
-					<li className={styles.menuItem}>
-						<button>Categorias</button>
+					<li className={styles.containerMenuItem}>
+						<button className={styles.menuItem} >Categorias</button>
 					</li>
-					<li className={styles.menuItem}>
-						<button>Meus Kits</button>
+					<li className={styles.containerMenuItem}>
+						<button className={styles.menuItem}>Meus Kits</button>
 					</li>
 				</ul>
 			</nav>
