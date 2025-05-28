88a0e7fbd6720077a25aad0a44fe186c806ffc12
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-05-27 23:36:07 -0300

    feat: desenvolve e estiliza o componente navBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,33 @@
+import { Link } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.logo}>
-                <img src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745342991/logo-novo-para-fundo-black_gbm10y.svg" alt="logo da empresa Momesso Brindes" />
-            </div>
+			<div className={styles.containerLogo}>
+				<img
+					className={styles.logo}
+					src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745342991/logo-novo-para-fundo-black_gbm10y.svg"
+					alt="logo da empresa Momesso Brindes"
+				/>
+			</div>
+			<nav className={styles.navMenu}>
+				<ul className={styles.listMenu}>
+					<li className={styles.menuItem}>
+						<button>Home</button>
+					</li>
+					<li className={styles.menuItem}>
+						<button>Categorias</button>
+					</li>
+					<li className={styles.menuItem}>
+						<button>Meus Kits</button>
+					</li>
+				</ul>
+			</nav>
+			<div className={styles.containerSearch}>
+				<label htmlFor="input">Pesquisar produtos:</label>
+				<input id="input" placeholder="🔍 Buscar produtos" type="text" />
+			</div>
 		</div>
 	);
 };
