a20b2a8d2a379dc43a998fe2fdca6bb68ad430b3
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-06-02 17:51:39 -0300

    fix: resolve bug do navBar que impedia o build

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styles from './NavBar.module.scss';
+import styles from './navBar.module.scss';
 import { useState } from 'react';
 import dataCategory from '../../data/DataCardsCategory.json';
 import dataHoliday from '../../data/holyDay.json';
