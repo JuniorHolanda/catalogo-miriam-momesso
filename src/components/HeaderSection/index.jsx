bfab2fac99eb66553ef8f1c7946f100fa7b4c82e
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-06-02 17:38:59 -0300

    feat: adiciona Link no logo Miriam Momesso do  HeaderSection, direcionando à Home

--- src/components/HeaderSection/index.jsx
+++ src/components/HeaderSection/index.jsx
@@ -1,10 +1,14 @@
 import styles from './headerSection.module.scss'
+import { Link } from 'react-router-dom';
 
 const HeaderSection = ({id , className}) =>{
   return (
     <header className={className}>
       <div className={styles.titleContainer}>
-        <img src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745342991/logo-novo-para-fundo-black_gbm10y.svg" alt="logo da empresa Momesso Brindes" />
+        
+        <Link to="/" className={styles.link}>
+          <img src="https://res.cloudinary.com/dnr3wfqyy/image/upload/v1745342991/logo-novo-para-fundo-black_gbm10y.svg" alt="logo da empresa Momesso Brindes" />
+        </Link>
         <h1>{id}</h1>
       </div>
     </header>
