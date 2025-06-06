913fbed9708531594979e0463e4bebcad53a3538
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-06-06 16:54:13 -0300

    style: ajusta o estilo de produtos

--- src/components/infoProduct/index.jsx
+++ src/components/infoProduct/index.jsx
@@ -1,6 +1,9 @@
 import styles from './infoProduct.module.scss';
-import { FaArrowUp, FaArrowRight, FaArrowDown, FaArrowLeft } from 'react-icons/fa';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 import MediaQuery from '../../utils/MediaQuery/MediaQuery';
+import likeAnimation from './Animation - 1748197599976.json'
+import Lottie from 'lottie-react';
+import { TfiDropbox } from 'react-icons/tfi';
 
 const InfoProduct = ({ name, category, measure, text }) => {
 	const isMobile = MediaQuery('(max-width: 400px)');
@@ -8,46 +11,52 @@ const InfoProduct = ({ name, category, measure, text }) => {
 	console.log(category);
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.containerTitle}>
-				<h2 className={styles.title}>{name}</h2>
-			</div>
-
-			<div className={styles.containercategory}>
+			<div className={styles.containerCatgory}>
 				{category.map((item) => (
 					<h2 key={item}>{item}</h2>
 				))}
 			</div>
 
-			<div className={styles.containerTitleMeasure}>
-				<h2>{measure >= 1 ? 'Medidas' : 'Medida'}</h2>
+			<div className={styles.containerToolBar}>
+				<button className={styles.btnKit}>
+					<TfiDropbox className={styles.icon}/>
+					Montar Kit
+				</button>
+				<button className={styles.btnLike}>
+					<Lottie animationData={likeAnimation} />
+				</button>
 			</div>
 
-			<div className={styles.containerMeasure}>
-				{isMobile && (
-					<div className={styles.containerIcon}>
-						<button className={styles.btnIcon}>
-							<FaArrowLeft />
-						</button>
-					</div>
-				)}
-
+			<div className={styles.containerInfo}>
+				<h2 className={styles.title}>{name}</h2>
+				<h2>{measure >= 1 ? 'Medidas' : 'Medida'}</h2>
 				<div className={styles.containerBtn}>
 					{measure.map((item) => (
 						<span key={item}>{item}</span>
 					))}
 				</div>
+				<div className={styles.containerText}>
+					<p>{text}</p>
+				</div>
+			</div>
 
-				{isMobile && (
-					<div className={styles.containerIcon}>
-						<button className={styles.btnIcon}>
-							<FaArrowRight />
-						</button>
-					</div>
-				)}
-			</div>
-			<div className={styles.containerText}>
-				<p>{text}</p>
-			</div>
+				<div className={styles.containerMeasure}>
+					{isMobile && (
+						<div className={styles.containerIcon}>
+							<button className={styles.btnIcon}>
+								<FaArrowLeft />
+							</button>
+						</div>
+					)}
+
+					{isMobile && (
+						<div className={styles.containerIcon}>
+							<button className={styles.btnIcon}>
+								<FaArrowRight />
+							</button>
+						</div>
+					)}
+				</div>
 		</section>
 	);
 };
