832ef665c7378c6c688d15126097fe2110d9f861
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-06-17 22:02:08 -0300

    fix: resolve o bug de persistÊncia de like na LocalStorage

--- src/components/CardSearch/index.jsx
+++ src/components/CardSearch/index.jsx
@@ -6,7 +6,6 @@ import Lottie from 'lottie-react';
 import animation from './Animation - 1748197599976.json';
 import cn from 'classnames';
 import { useEffect, useRef, useState } from 'react';
-import { deleteSotorage, getSotorage, setSotorage } from '../../utils/MediaQuery/localStorage/localSorage';
 
 const CardSearch = ({ product }) => {
 	const slug = slugify(product.title, { lower: true, strict: true });
@@ -17,25 +16,27 @@ const CardSearch = ({ product }) => {
 		return storedLike === 'true'; // converte para booleano
 	});
 
-	
+	useEffect(() => {
+		if (like) {
+			animationRef.current?.play();
+		}
+	}, []);
 
 	// animação do btn like
 	const handleLike = () => {
 		const current = !like;
-
 		setLike(current);
-
 		// Se já está rodando, pode parar ou reiniciar:
 		if (current) {
 			animationRef.current?.play();
-			console.log('criando valor na localStorage', setSotorage(product._id, current));
+			localStorage.setItem(`like${product._id}`, 'true');
 		} else {
 			animationRef.current?.stop();
-			console.log('removendo dados da localStorage', deleteSotorage(product._id));
+			localStorage.removeItem(`like${product._id}`);
 		}
 	};
 
-//define a classe do cartão combase na quantidade de like
+	//define a classe do cartão combase na quantidade de like
 	const statusClass = cn(styles.containerContent, {
 		[styles.status1]: product.like >= 0 && product.like < 5,
 		[styles.status2]: product.like >= 5 && product.like < 10,
@@ -48,7 +49,11 @@ const CardSearch = ({ product }) => {
 				<div className={styles.headerCard}>
 					<div className={styles.containerCategoryCard}>
 						{product.category.map((item) => {
-							return <span className={styles.categoryCard}>{item}</span>;
+							return (
+								<span className={styles.categoryCard}>
+									{item}
+								</span>
+							);
 						})}
 					</div>
 					<Lottie
@@ -69,7 +74,10 @@ const CardSearch = ({ product }) => {
 						<h2>{product.title}</h2>
 						<p>{product.smallText}</p>
 					</div>
-					<Link to={`/product/${slug}`} className={styles.containerBtn}>
+					<Link
+						to={`/product/${slug}`}
+						className={styles.containerBtn}
+					>
 						<span>
 							<LiaEyeSolid className={styles.icon} />
 							Ver Produto
