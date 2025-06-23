00dd6a73c709d40fa08ca802ba66aff566ab1f2f
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-06-23 13:17:09 -0300

    feat: modificações no componente kit
    adiciona loader, adiciona resposta em caso de não haver kit, melhora a responsividade do componente

--- src/Pages/Kit/index.jsx
+++ src/Pages/Kit/index.jsx
@@ -1,13 +1,18 @@
 import styles from './kit.module.scss';
 import CardSearch from '../../components/CardSearch';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getProducts } from '../../services/productsMomessoServices';
+import LoaderData from '../../components/Loader';
+import Lottie from 'react-lottie-player';
+import animation from '../../animation/animation-kit.json'
 
 const MyKit = () => {
 	const allKeyStorage = Object.keys(localStorage);
 	const kitKeys = allKeyStorage.filter((key) => key.includes('kit'));
 	const productId = kitKeys.map((key) => key.replace('kit', ''));
 	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const animationRef = useRef()
 
 	useEffect(() => {
 		async function fetchProducts() {
@@ -15,7 +20,9 @@ const MyKit = () => {
 				const response = await getProducts();
 				setProducts(response);
 			} catch (error) {
-				console.log('Erro ao carregar produtos:', error.message);
+				console.error('Erro ao buscar produtos:', error);
+			} finally {
+				setLoading(false);
 			}
 		}
 		fetchProducts();
@@ -26,11 +33,37 @@ const MyKit = () => {
 
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.containerProducts}>
-				{kitProducts.map((prod) => (
-					<CardSearch key={prod._id} product={prod} />
-				))}
-			</div>
+			{loading ? (
+				<div className={styles.loaderContainer}>
+					<LoaderData />
+				</div>
+			) : (
+				<div className={styles.content}>
+					{kitProducts.length === 0 ? (
+						<div className={styles.containerText}>
+							<div>
+								<h2 className={styles.title}>Nada por aqui ainda.</h2>
+								<p className={styles.text}>Explore os produtos e use o ícone de kit para montar o seu.</p>
+							</div>
+							<div>
+								<Lottie
+									ref={animationRef}
+									play={true}
+									animationData={animation}
+									loop={true}
+									style={{ height: '50px' }}
+								/>
+							</div>
+						</div>
+					) : (
+						<div className={styles.containerProducts}>
+							{kitProducts.map((prod) => (
+								<CardSearch key={prod._id} product={prod} />
+							))}
+						</div>
+					)}
+				</div>
+			)}
 		</section>
 	);
 };
