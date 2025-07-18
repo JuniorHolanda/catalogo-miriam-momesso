dffc33fcbf2dc025af3a01c0e57c5f5a8b026e3a
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-07-18 10:45:06 -0300

    style: estiliza o componente de cardImported

--- src/components/cardImported/index.jsx
+++ src/components/cardImported/index.jsx
@@ -4,8 +4,16 @@ import BtnInteractive from '../btn/btnInteractive';
 import animationLike from '../../animation/animation-like.json';
 import animationFavorite from '../../animation/animation-favorite.json';
 import animationKit from '../../animation/animation-kit.json';
+import { useState } from 'react';
+import { FaArrowDown, FaArrowUp, FaWhatsapp } from 'react-icons/fa';
 
 const CardImported = ({ product }) => {
+	const description = product.descricao;
+	const [showMorty, setShowMorty] = useState(false);
+	const limitedDescription = description.length > 150 ? `${description.slice(0, 120)}...` : description;
+	const mensage = `Olá, gostaria de saber mais sobre o produto ${product.codigoAmigavel} `;
+	const codedMensage = encodeURIComponent(mensage);
+
 	return (
 		<div className={styles.cardItem}>
 			<div className={styles.headerCard}>
@@ -26,13 +34,26 @@ const CardImported = ({ product }) => {
 					/>
 				</div>
 			</div>
-			<div className={styles.containerThunb}>
+			<div className={showMorty ? styles.hideContainerThunb : styles.showContainerThunb}>
 				<img src={product.imageLink} alt={product.descricao} />
 			</div>
 
 			<div className={styles.containerName}>
 				<h2 className={styles.title}>{product.nome}</h2>
-				<p className={styles.text}>{product.descricao}</p>
+				<p className={styles.text}>{showMorty ? description : limitedDescription}</p>
+				<div className={styles.containerBtn}>
+					{description.length > 150 && (
+						<button className={styles.btnShowMory} onClick={() => setShowMorty(!showMorty)}>
+							{showMorty ? <FaArrowUp /> : <FaArrowDown />}
+						</button>
+					)}
+					<div className={styles.containerWhatsApp}>
+						<FaWhatsapp className={styles.icon}/>
+						<a href={`https://api.whatsapp.com/send?phone=5511974783545&text=${codedMensage}` }className={styles.link}>
+							Orçamento
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
